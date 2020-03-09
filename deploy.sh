cc90e3a6ea821da6eec05b81a4bdade07f719e0d
温晓阳 <wxy_hhy@163.cmo>
2020-03-09 16:27:50 +0800

    1

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run build
+npm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -13,7 +13,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-git push -f https://github.com/tskii/buyathom.git master
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:olewaHHH/blog-demo.git master:gh-pages
 
 cd -
